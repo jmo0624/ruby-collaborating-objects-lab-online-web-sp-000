f728a07ae55f051ea813f603fbd0801914419ed4
Josh Morris <jmo0624@gmail.com>
2019-07-12 01:30:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,5 +7,5 @@ class Song
   end
   
   def self.new_by_filename(file_name)
-    song = file_name.split" - "[1]
-    artist = file_name.split
+    song = file_name.split(" - ")[1]
+    artist = file_name.split(" - ")[]
