0f0e7c18a72d03f33b0ed92a0b11f0516c5e61b7
Josh Morris <jmo0624@gmail.com>
2019-07-12 01:42:08 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,17 +1,21 @@
-class MP3_Importer
-  
+class MP3Importer
   attr_accessor :path
-  
+
   def initialize(path)
     @path = path
   end
-  
+
   def files
-    files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
+    files = []
+    Dir.new(self.path).each do |file|
+      files << file if file.length > 4
+    end
+    files
   end
-  
+
   def import
-    files.each {|song| Song.new_by_filename(song)}
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end
   end
-  
 end
